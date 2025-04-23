a9bea7355205e12fb4aad5290f744624e69f0baa
Bexcelsus <fukudarikuto.24@gmail.com>
2025-04-23 15:16:49 +0900

    問題を構造体にして増やした。

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,17 +1,35 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-int main() {
-	cout << "[リクルート試験対策クイズ]\n";
-	cout << "13×(-5)の答えは?\n";
+//問題と答えを纏める構造体
+struct Question
+{
+	string q;	//問題文
+	int a;		//答え
+};
 
-	int answer;
-	cin >> answer;
-	if (answer == 13 * -5) {
-		cout << "正解！\n";
-	}
-	else
+int main() {
+	Question questions[] = {
+		{"13×(-5)の答えは？",13 * -5},
+		{"(-21)÷(-3)",-21 / -3},
+		{"7-(4+2)÷2",7 - (4 + 2) / 2},
+	};
+
+	cout << "[リクルート試験対策クイズ]\n";
+
+	for (const auto& e : questions)
 	{
-		cout << "間違い！正解は" << 13 * -5 << "\n";
-	}
+		cout << e.q << "の答えは ? \n";
+		int answer;
+		cin >> answer;
+		if (answer == e.a) {
+			cout << "正解！\n";
+		}
+		else
+		{
+			cout << "間違い！正解は" << e.a << "\n";
+		}
+	}	// for questions
+
 }
