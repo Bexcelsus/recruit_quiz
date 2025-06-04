ad230c6fe7c7533f0cb7df112136d7da04436213
Bexcelsus <fukudarikuto.24@gmail.com>
2025-06-04 14:57:04 +0900

    慣用句を答える問題を追加した

--- recruit_quiz/exam_japanese.h
+++ recruit_quiz/exam_japanese.h
@@ -4,5 +4,8 @@
 //Š¿š‚Ì“Ç‚İæ‚è–â‘è‚ğì¬‚·‚é
 QuestionList CreateKanjiExam();
 
+//Šµ—p‹å‚ÌˆÓ–¡‚ğ“š‚¦‚é–â‘è‚ğì¬‚·‚é
+QuestionList CreateIdiomExam();
+
 //“¯‚¶“Ç‚İ‚ÅˆÓ–¡‚ÌˆÙ‚È‚éŒê‚Ì–â‘è‚ğì¬‚·‚é
 QuestionList CreateHomophoneExam();
--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -102,6 +102,8 @@ int main() {
 	cin >> subject;
 	if (subject == 2) {
 		questions = CreateKanjiExam();
+		const QuestionList idiomExam = CreateIdiomExam();
+		questions.insert(questions.end(), idiomExam.begin(), idiomExam.end());
 	}
 
 	for (const auto& e : questions)
