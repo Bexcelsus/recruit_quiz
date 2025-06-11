6b6636ad67f0ee631961d8b24a9185e60a314dc5
Bexcelsus <fukudarikuto.24@gmail.com>
2025-06-11 14:59:45 +0900

    英単語の問題を追加した

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,5 +1,6 @@
 #include"question.h"
 #include"exam_japanese.h"
+#include"exam_english.h"
 
 #include<iostream>
 #include<string>
@@ -97,7 +98,7 @@ int main() {
 
 	cout << "[リクルート試験対策クイズ]\n";
 
-	cout << "教科を選んでください\n1=数学\n2=国語\n";
+	cout << "教科を選んでください\n1=数学\n2=国語\n\,n3 =英語nx";
 	int subject;
 	cin >> subject;
 	if (subject == 2) {
@@ -110,6 +111,9 @@ int main() {
 		questions.insert(questions.end(), antonymExam.begin(), antonymExam.end());
 		QuestionList synonymExam = CreateSynonymExam();
 		questions.insert(questions.end(), synonymExam.begin(), synonymExam.end());
+	}
+	else if (subject == 3) {
+		questions = CreateEnglishWordExam();
 	}
 
 	for (const auto& e : questions)
