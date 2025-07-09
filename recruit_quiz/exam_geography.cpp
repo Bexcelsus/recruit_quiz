b0c5c1a775ad761421e82eded047f6a4f786584c
Bexcelsus <fukudarikuto.24@gmail.com>
2025-07-09 16:07:02 +0900

    地理追加

--- recruit_quiz/exam_geography.cpp
+++ recruit_quiz/exam_geography.cpp
@@ -77,9 +77,47 @@ QuestionList CreatePrefecturesExam() {
 		}
 		break;
 	case 1:	//都道府県を表す特徴を答える
+		for (int i = 0; i < quizCount; i++) {
+			//間違った番号をランダムに選ぶ
+			const int correctIndex = indices[i];
+			vector<int> answers = CreateWrongIndices((int)data.size(), correctIndex);
+
+			//ランダムな位置を正しい番号で上書き
+			const int correctNo = uniform_int_distribution<>(1, 3)(rd);
+			answers[correctNo - 1] = correctIndex;
+
+			//問題文を作成
+			string s = "「"+data[correctIndex].name + "」の特徴を表す番号を選べ";
+			for (int j = 0; j < 3; j++)
+			{
+				const auto& features = data[answers[j]].features;
+				const int n = uniform_int_distribution<>(0, (int)features.size() - 1)(rd);
+				s += "\n " + to_string(j + 1) + ":" + features[n];
+			}
+			questions.push_back({ s, to_string(correctNo) });
+		}
 		break;
 	case 2:	//都道府県から県庁所在地を答える
+	{
+			//都道府県名と県庁所在地が異なる都道府県のリストを作る
+		vector<pair<string, string>>differentNames;
+		for (const auto& e : data)
+		{
+			//都道府県名と県庁所在地名の長さが異なるか、末尾の1文字を除く部分文字列が異なる場合、
+			//「都道府県名と県庁所在地が異なるリスト」に追加する	
+			if (e.name.size() != e.capital.size() ||
+				memcmp(e.name.data(), e.capital.data(), e.name.size() - 2) != 0) {
+				differentNames.push_back({ e.name,e.capital });
+			}
+		}
+		//作成したリストからランダムに出題する
+		vector<int>indices = CreateRandomIndices((int)differentNames.size());
+		for (int i = 0; i < quizCount; i++) {
+			const int correctIndex = indices[i];
+			questions.push_back({ "「" + differentNames[correctIndex].first + "」の県庁所在地を答えよ", differentNames[correctIndex].second });
+		}
 		break;
+	}
 	}
 
 	return questions;
