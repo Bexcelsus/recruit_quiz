ab72b0fb7df0add38af55137cd81eb394a5b916a
Bexcelsus <fukudarikuto.24@gmail.com>
2025-07-30 16:10:58 +0900

    全角数字が半角数字に自動で変換されるようにした。

--- recruit_quiz/utility.h
+++ recruit_quiz/utility.h
@@ -15,3 +15,7 @@ std::vector<int> CreateWrongIndices(int n, int correctIndex);
 
 //•¶š—ñ‚ğ•ªŠ„‚·‚é
 std::vector<std::string> Split(const std::string& s, char c);
+
+//SJIS”’l•¶š—ñ‚ğASCII”’l•¶š—ñ‚É•ÏŠ·‚·‚é
+//sjis •ÏŠ·Œ³‚Ìsjis•¶š—ñ
+std::string ConvertSjisNumberToAscii(const std::string& sjis);
